380547e246a9771a0067977be7e708bde8862a2a
agnesF <agnes.anglade@free.fr>
2016-11-22 16:42:19 +0100

    nouveau repo sur htdocs en local

---
+++ essaiEcho.php
@@ -0,0 +1,13 @@
+<html>
+    <head> <title> essai echo</title>
+    </head>
+    <body>
+        <h1> essai echo</h1>
+        <?php
+            //http://localhost:8888/SI4/somme2nbHtml.php?nb1=10&nb2=12
+         
+            echo "Ceci est un ","test de echo";
+            echo "rajout d'une ligne";
+        ?>
+    </body>
+</html>
